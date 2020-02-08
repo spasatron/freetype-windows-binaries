e11b92d7f173e23d4a88bae61bddf6b7434cb9e2
Spas Angelov <spasangelov@gmail.com>
2020-02-07 20:38:26 -0700

    added premake

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "FreeType"
+	kind "SharedLib"
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "include/**.h"
+	}
+
+    includedirs{
+        "include/"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
